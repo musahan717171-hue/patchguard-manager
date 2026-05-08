01c738ac0da3374b8180f588b87468d733e45de8
musahan717171-hue <musahan717171@gmail.com>
2026-05-08 17:56:42 +0500

    Update settings.py
    fix static root

--- patchguard_manager/settings.py
+++ patchguard_manager/settings.py
@@ -67,7 +67,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.environ.get("STATIC_ROOT", str(BASE_DIR / "staticfiles"))
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "login"
